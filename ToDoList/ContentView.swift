231600422c4a76bfa083ea44e6fd79f2c09b6600
Elizabeth Saka <esaka28@andover.edu>
2025-08-08 13:09:26 -0400

    Added Text and a Button

--- ToDoList/ContentView.swift
+++ ToDoList/ContentView.swift
@@ -10,14 +10,24 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
+            HStack{
+                Text("To Do List")
+                    .font(.system(size:40))
+                    .fontWeight(.heavy)
+                Spacer()
+                Button {
+                } label: {
+                    Text("+")
+                        .font(.title)
+                        .fontWeight(.bold)
+                }
+                
+            }//end of hstack
+            Spacer()
+        }//end of VStaack
         .padding()
-    }
-}
+    }//end of var body: some View
+}// end of view
 
 #Preview {
     ContentView()
